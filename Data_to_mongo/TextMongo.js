996eebe80437647ac15f23e1956c6a59919bc71b
antrot3 <antrot3@gmail.com>
2016-12-08 19:06:37 +0100

    USer manual update

--- Data_to_mongo/TextMongo.js
+++ Data_to_mongo/TextMongo.js
@@ -1,6 +1,6 @@
 conn= new Mongo();
-db= conn.getDB("Diplomski");
-var file= cat('LT_db_M1_2012_11_19.txt')
+db= conn.getDB("symmetrybaza"); // add database name here or replace with symmetrybaza
+var file= cat('LT_db_M1_2012_11_19.txt') //use diffrent txt file to manimulate it
 var file2=file.slice(11);
 var array= file2.split(/(\s+)/);
 print(array.length);
@@ -34,7 +34,7 @@ db["things"].insert({"name":col[0],[name[0].substring(lenght0+1)] : broj[0]
 ,[name[1].substring(lenght0+1)] : broj[1]
 ,[name[2].substring(lenght0+1)] : broj[2]
 ,[name[3].substring(lenght0+1)] : broj[3]
-,[name[4].substring(lenght0+1)] : broj[4],"telescope":"M1"
+,[name[4].substring(lenght0+1)] : broj[4],"telescope":"M1" //if there is more fields in database add [name[5].substring(lenght0+1)] : broj[5] and more if needed
  });
 
 
